26238ee552204993a90413d7cb3fd0242d38e314
Rogan Grant <rogangrant2022@u.northwestern.edu>
2019-02-05 15:22:55 -0600

    Updated GNU parallel flags for installed version

--- 190131_DEA_pipeline.sh
+++ 190131_DEA_pipeline.sh
@@ -57,8 +57,8 @@ then
 	--fastqc -o ./fastqc/after_trim ::: ./raw_data/*.fastq.gz
 	multiqc ./fastqc/after_trim/* -o ./fastqc/after_trim/
 else
-	#--link only runs pairs, as opposed to every combination of outputs
-	parallel --link -j $THREADS trim_galore --paired \
+	#--xapply only runs pairs, as opposed to every combination of outputs
+	parallel --xapply -j $THREADS trim_galore --paired \
 	--quality $QCUTOFF --phred33 -o ./trimmed \
 	--fastqc --fastqc_args "-outdir ./fastqc/after_trim" \
 	::: ./raw_data/*_1.fastq.gz ::: ./raw_data/*_2.fastq.gz
